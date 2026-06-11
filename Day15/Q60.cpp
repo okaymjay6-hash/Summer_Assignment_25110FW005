295b91866ed6484190334228403b77d830e3fd28
MJay <okaymjay6@gmail.com>
2026-06-12 00:29:25 +0530

    Q60 updated

--- Day15/Q60.cpp
+++ Day15/Q60.cpp
@@ -6,22 +6,20 @@ int main(){
     int n;
     cout << "Enter array size: ";
     cin >> n;
-    int n1=n;
     int arr[n];
     cout << "Enter elements: ";
     for(int i=0; i<n; i++){
         cin >> arr[i];
     }
 
+    int j=0;
     for(int i=0; i<n; i++){
-        if(arr[i]==0){
-        for(int j=i; j<n-1; j++)
-        swap(arr[j],arr[j+1]);
-      }
-      n--;
-      i--;
+        if(arr[i]!=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
     }
-    for(int i=0; i<n1; i++){
+    for(int i=0; i<n; i++){
         cout << arr[i] << " ";
     }
     return 0;
