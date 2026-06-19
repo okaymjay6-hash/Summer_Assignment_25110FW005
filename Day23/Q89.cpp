e7b82dee2324f724298a1807816b93e5bf39f998
MJay <okaymjay6@gmail.com>
2026-06-19 23:35:38 +0530

    Day23 uploaded

---
+++ Day23/Q89.cpp
@@ -0,0 +1,29 @@
+//first non-repeating character
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+void non_repeating(string str){
+    int freq[26]={0};
+    queue<char> Q;
+    for(int i=0; i<str.size(); i++){
+        char ch=str[i];
+        freq[ch -'a']++;
+        Q.push(ch);
+        while(!Q.empty() && freq[Q.front()-'a']>1){
+            Q.pop();
+        }
+        if(Q.empty()){
+            cout << -1 << " ";
+        }else{
+            cout << Q.front() << " ";
+        }
+    }
+}
+
+int main(){
+    string str="aabccxb";
+    non_repeating(str);
+    return 0;
+}
